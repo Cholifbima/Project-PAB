ae37768707f9fed2e123c2c4f6c3ed5da536ec5d
fthndhym <fathandhym@gmail.com>
2025-06-01 18:54:09 +0700

    hapus useless ThemeManager.kt

--- app/src/main/java/com/example/pabproject/utils/ThemeManager.kt
+++ app/src/main/java/com/example/pabproject/utils/ThemeManager.kt
@@ -1,18 +1,13 @@
 package com.example.pabproject.utils
 
-import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
 
 class ThemeManager : ViewModel() {
     // Use backing properties to avoid name conflicts with setter methods
     private val _isDarkMode = mutableStateOf(false)
     val isDarkMode: Boolean get() = _isDarkMode.value
-    
-    private val _highContrastMode = mutableStateOf(false)
-    val highContrastMode: Boolean get() = _highContrastMode.value
-    
+
     fun toggleDarkMode() {
         _isDarkMode.value = !_isDarkMode.value
     }
@@ -20,8 +15,5 @@ class ThemeManager : ViewModel() {
     fun setDarkMode(enabled: Boolean) {
         _isDarkMode.value = enabled
     }
-    
-    fun setHighContrastMode(enabled: Boolean) {
-        _highContrastMode.value = enabled
-    }
-} 
+
+}
